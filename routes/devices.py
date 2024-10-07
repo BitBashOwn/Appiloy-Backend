b6208b5aef111237d519c8773f746007a5973ccf
Sohaib-2 <sohaibmaqsood63@gmail.com>
2024-10-07 23:20:45 +0500

    reslove typerror issue (mongoobject id)

--- routes/devices.py
+++ routes/devices.py
@@ -15,19 +15,38 @@ class deleteRequest(BaseModel):
     devices: list
 
 
+# @devices_router.get("/devices")
+# async def get_devices(current_user: dict = Depends(get_current_user)):
+#     # print(current_user)
+#     # data = list(devices_collection.find({"email": current_user.get("email")}, {"_id": 0}))
+#     data = list(db["devices"].find({"email": current_user.get("email")}))
+
+#     # for device in data:
+#     #     if '_id' in device:
+#     #         device['_id'] = str(device['_id'])
+
+#     # devices = json.dumps(data)
+#     devices = data
+#     return JSONResponse(content={"devices": devices}, status_code=200)
+# Helper function to convert MongoDB ObjectId to string
+def convert_object_id(data):
+    if isinstance(data, list):
+        for item in data:
+            item['_id'] = str(item['_id'])
+    elif isinstance(data, dict):
+        data['_id'] = str(data['_id'])
+    return data
+
 @devices_router.get("/devices")
 async def get_devices(current_user: dict = Depends(get_current_user)):
-    # print(current_user)
-    # data = list(devices_collection.find({"email": current_user.get("email")}, {"_id": 0}))
+    # Query the devices for the current user from MongoDB
     data = list(db["devices"].find({"email": current_user.get("email")}))
 
-    # for device in data:
-    #     if '_id' in device:
-    #         device['_id'] = str(device['_id'])
+    # Convert ObjectId fields to strings for JSON serialization
+    data = convert_object_id(data)
 
-    # devices = json.dumps(data)
-    devices = data
-    return JSONResponse(content={"devices": devices}, status_code=200)
+    return JSONResponse(content={"devices": data}, status_code=200)
+
 
 
 @devices_router.delete("/delete-devices")
