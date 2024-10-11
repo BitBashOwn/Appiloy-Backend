146d3e279f45536e07b368c2abf401c533036fd3
AbdullahNoor-codes <abdullahnoor94@gmail.com>
2024-10-12 01:41:56 +0500

    changed /send_Command

--- routes/deviceRegistration.py
+++ routes/deviceRegistration.py
@@ -8,8 +8,7 @@ from models.tasks import tasks_collection
 
 
 # MongoDB Connection
-client = MongoClient(
-    "mongodb+srv://abdullahnoor94:dodge2018@appilot.ds9ll.mongodb.net/?retryWrites=true&w=majority&appName=Appilot")
+client = MongoClient("mongodb+srv://abdullahnoor94:dodge2018@appilot.ds9ll.mongodb.net/?retryWrites=true&w=majority&appName=Appilot")
 db = client["Appilot"]
 device_collection = db["devices"]
 
@@ -21,8 +20,6 @@ active_connections: List[WebSocket] = []
 device_connections = {}  # To store device_id: websocket mapping
 
 # Device registration model with updated fields
-
-
 class DeviceRegistration(BaseModel):
     deviceName: str
     deviceId: str
@@ -33,38 +30,29 @@ class DeviceRegistration(BaseModel):
     email: str
 
 # Command model
-
-
 class CommandRequest(BaseModel):
     command: dict
     device_ids: List[str]
-
+    
 # class CommandRequest(BaseModel):
 #     command: dict
 #     device_ids: List[str]
 #     task: dict
 
 # Dependency for registering and validating the device
-
-
 def register_device(device_data: DeviceRegistration):
     device = device_collection.find_one({"deviceId": device_data.deviceId})
     if device:
-        raise HTTPException(
-            status_code=400, detail="Device already registered")
+        raise HTTPException(status_code=400, detail="Device already registered")
     result = device_collection.insert_one(device_data.dict())
     return {"message": "Device registered successfully", "deviceId": device_data.deviceId}
 
 # Device Registration Endpoint (POST)
-
-
 @device_router.post("/register_device")
 async def register_device_endpoint(device_data: DeviceRegistration):
     return register_device(device_data)
 
 # Endpoint to check the device status
-
-
 @device_router.get("/device_status/{device_id}")
 async def check_device_status(device_id: str):
     device = device_collection.find_one({"deviceId": device_id})
@@ -73,30 +61,24 @@ async def check_device_status(device_id: str):
     return {"device_id": device_id, "status": device["status"]}
 
 # Endpoint to update the device status
-
-
 @device_router.put("/update_status/{device_id}")
 async def update_device_status(device_id: str, status: bool):
     device = device_collection.find_one({"deviceId": device_id})
     if not device:
         raise HTTPException(status_code=404, detail="Device not found")
-    device_collection.update_one({"deviceId": device_id}, {
-                                 "$set": {"status": status}})
+    device_collection.update_one({"deviceId": device_id}, {"$set": {"status": status}})
     return {"message": f"Device {device_id} status updated to {status}"}
 
 # Endpoint for checking device registration
-
-
 @device_router.get("/device_registration/{device_id}")
 async def check_device_registration(device_id: str):
     device = device_collection.find_one({"deviceId": device_id})
     if not device:
         raise HTTPException(status_code=404, detail="Device not found")
-
+    
     if not device["status"]:
-        device_collection.update_one({"deviceId": device_id}, {
-                                     "$set": {"status": True}})
-
+        device_collection.update_one({"deviceId": device_id}, {"$set": {"status": True}})
+    
     return True
 
 # # WebSocket endpoint
@@ -110,7 +92,7 @@ async def check_device_registration(device_id: str):
 #         while True:
 #             data = await websocket.receive_text()
 #             print(f"Message from {device_id}: {data}")
-
+            
 #             for connection in active_connections:
 #                 if connection != websocket:
 #                     await connection.send_text(f"Message from {device_id}: {data}")
@@ -119,38 +101,34 @@ async def check_device_registration(device_id: str):
 #         active_connections.remove(websocket)
 #         device_connections.pop(device_id, None)
 # WebSocket endpoint
-
-
 @device_router.websocket("/ws/{device_id}")
 async def websocket_endpoint(websocket: WebSocket, device_id: str):
     await websocket.accept()
     device_connections[device_id] = websocket
     active_connections.append(websocket)
-
+    
     try:
         # Update device status to true when connected
         # device_collection.update_one({"deviceId": device_id}, {"$set": {"status": True}})
-
+        
         while True:
             data = await websocket.receive_text()
             print(f"Message from {device_id}: {data}")
             for connection in active_connections:
                 if connection != websocket:
                     await connection.send_text(f"Message from {device_id}: {data}")
-
+                    
     except WebSocketDisconnect:
         print(f"Device {device_id} disconnected.")
         # Update device status to false when disconnected
-        device_collection.update_one({"deviceId": device_id}, {
-                                     "$set": {"status": False}})
+        device_collection.update_one({"deviceId": device_id}, {"$set": {"status": False}})
         active_connections.remove(websocket)
         device_connections.pop(device_id, None)
 
 # Command Endpoint
-
-
 @device_router.post("/send_command")
 async def send_command(request: CommandRequest):
+    print(request)
     device_ids = request.device_ids
     command = request.command
     not_connected_devices = []
@@ -163,8 +141,10 @@ async def send_command(request: CommandRequest):
             not_connected_devices.append(device_id)
 
     if not_connected_devices:
+        print("error")
         result = list(db["devices"].find(
             {"deviceId": {"$in": not_connected_devices}}, {"deviceName": 1, "_id": 0}))
+        print(result)
         raise HTTPException(status_code=404, detail={
             "error": f"Devices with IDs {not_connected_devices} are not connected.",
             "devices": result
